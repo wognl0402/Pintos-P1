a0eada4e988b2606508498916dd3a9029271e2f6
wognl0402 <wognl0402@kaist.ac.kr>
2018-09-16 07:46:49 +0000

    Initial Files for Project1

---
+++ src/tests/threads/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void test_hello_world(void){
+    printf("hello world!");
+    return;
+}
---
+++ src/threads/build/tests/threads/hello.d
@@ -0,0 +1,3 @@
+tests/threads/hello.o: ../../tests/threads/hello.c ../../lib/stdio.h \
+  ../../lib/debug.h ../../lib/stdarg.h ../../lib/stdbool.h \
+  ../../lib/stddef.h ../../lib/stdint.h ../../lib/kernel/stdio.h
